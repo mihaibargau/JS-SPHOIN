4273b51410d8ae058abb9c79b33e30c962d45986
Mihai Bargau <mihaibargau@gmail.com>
2019-04-01 13:57:59 +0300

    new pages

---
+++ forgot-password.js
@@ -0,0 +1,20 @@
+function forgotPassword()
+{
+    var email = document.getElementById("email").value;
+    var xhr = new XMLHttpRequest();
+    var method = 'GET';
+    var url = 'https://cors.io/?http://54.211.9.51:8000';
+    xhr.open(method, url + '/' + 'user_forgot' + '/' + email + '/');
+    console.log(url + '/' + 'user_forgot' + '/' + email + '/');
+    xhr.send();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            if (response.status == 'email_pass_recovery_sent')
+            alert("Recovery password sent");
+            else
+            alert("User was not found");
+            
+        }
+    };
+}
--- sign-up.js
+++ sign-up.js
@@ -1,19 +1,22 @@
-function getInput()
+function signUp()
 {
     var user = document.getElementById("username").value;
     var pass = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
     var xhr = new XMLHttpRequest();
     var method = 'GET';
     var url = 'https://cors.io/?http://54.211.9.51:8000';
-    xhr.open(method, url + '/user_login' + '/' + user + '/' + pass + '/');
+    xhr.open(method, url + '/' + 'user_new' + '/' + user + '/' + pass + '/' + email + '/');
+    console.log(method, url + '/' + 'user_new' + '/' + user + '/' + pass + '/' + email + '/');
     xhr.send();
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var response = JSON.parse(xhr.responseText);
-            if (response.status == 'logged_in')
-            window.location.assign('dashboard.html');
+            
+            if (response.status == 'registered')
+                alert("User registration successful");
             else 
-                 alert("Invalid credentials");
+                alert("User already exists");
             
         }
     };
